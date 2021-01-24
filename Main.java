2e4dc1dbf2fedfddfa61e4bdb1b31d97a3bed904
diegotco <diegotco@yahoo.com>
2021-01-24 12:48:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+
+  int numeroChico = 10000;
+  int numeroGrande = 1000;
+        
+  if(numeroChico < numeroGrande){
+  System.out.println("El " + numeroChico + " es menor a "+ numeroGrande);
+  }
+  else{
+  System.out.println("En realidad el número chico es mayor al número grande");
+  }
+        
+  System.out.println("Hola Mundo");
+   
+  }
+}
